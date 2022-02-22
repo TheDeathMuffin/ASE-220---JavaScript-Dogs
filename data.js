73ec648e8bac682d913d0ab3fa4c53ad0b62342f
Angle-M <98354046+Angle-M@users.noreply.github.com>
2022-02-22 11:59:46 -0500

    Javascript update
    It is almost all setup, it needs the bio and whatever else you all need me to setup in the script

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-var dogs=[
+var pets=[
 			{
 				"name":"Wishbone"
 				"breed":"American Bulldog cross"
@@ -54,6 +54,115 @@ var dogs=[
 			}
 
 			];
+
+var details=[
+			{
+				"name":"Wishbone"
+				"Type":"Dog"
+				"breed":"American Bulldog cross"
+				"spayed":"Spayed / Neutered"
+				"gender":"Male"
+				"color":"Black"
+				"age":"2Yrs 1Mths 2Wks (approx)"
+				"size":"Large"
+				"health":"My health has been checked"
+				"vac":"My vaccinations are up to date"
+				"worming":"My worming is up to date"
+				"mircochip":"I have been microchipped"
+				"animal ID":"20579"
+				"micro Number":"985113004709947"
+				"bio":""
+			},
+
+
+			{
+				"name":"Benny"
+				"Type":"Dog"
+				"breed":"Australian Shepherd cross"
+				"spayed":"Spayed / Neutered"
+				"gender":"Male"
+				"color":"Brown / White"
+				"age":"2Yrs 1Mths 4Wks (approx)"
+				"health":"My health has been checked"
+				"vac":"My vaccinations are up to date"
+				"worming":"My worming is up to date"
+				"mircochip":"I have been microchipped" 
+				"animal ID":"23531"
+				"micro Number":"985113005872810"
+				"suit guide":"Not suitable for Children"
+			},
+
+			{
+				"name":"Cornelia"
+				"Type":"Dog"
+				"breed":"Boxer cross"
+				"spayed":"Spayed / Neutered"
+				"gender":"Female"
+				"color":"White"
+				"age":"6Yrs 2Mthns 3Wks (approx)"
+				"health":"My health has been checked"
+				"vac":"My vaccinations are up to date"
+				"worming":"My worming is up to date"
+				"mircochip":"I have been microchipped"
+				"animal ID":"23390"
+				"micro Number":"985113005872687"
+				"suit guide":"OK with Dogs" "Laid back" "Shy"/*need to figure out how to have these on the same line*/
+				"bio":""
+			},
+			{
+				"name":"Niko"
+				"Type":"Cat"
+				"breed":"Domestic Longhair cross"
+				"spayed":"Spayed / Neutered"
+				"gender":"Male"
+				"color":"Grey tabby / White"
+				"claw":"Declawed"
+				"age":"3 Mths 3Wks"
+				"health":"My health has been checked"
+				"vac":"My vaccinations are up to date"
+				"worming":"My worming is up to date"
+				"mircochip":"I have been microchipped"
+				"animal ID":"23541"
+				"micro Number":"985113006299812"
+				"bio":""
+			},
+
+			{
+				"name":"Howard"
+				"Type":"Cat"
+				"breed":"Domestic Short Hair Mixed Russian"
+				"second Breed":"Russian"
+				"spayed":"Spayed / Neutered"
+				"gender":"Male"
+				"color":"Grey"
+				"claw":"No"
+				"age":"Adult"
+				"health":"My health has been checked"
+				"vac":"My vaccinations are up to date"
+				"worming":"My worming is up to date"
+				"mircochip":"I have been microchipped"
+				"animal ID":"23882"
+				"mircochip Number":"985113006299853"
+				"bio":""
+			},
+
+			{
+				"name":"Hemingway"
+				"Type":"Cat"
+				"breed":"Domestic Short Hair cross"
+				"Spayed / Neutered":"Yes"
+				"gender":"Male"
+				"color":"Grey"
+				"claw":"No"
+				"age":"3Yrs 3Wks (approx)"
+				"health":"My health has been checked"
+				"vac":"My vaccinations are up to date"
+				"worming":"My worming is up to date"
+				"animal ID":"23897"
+				"suit guide":"Doesn't like Cats","Special Needs"
+				"bio":""
+			}
+];
 
 			
 

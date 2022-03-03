59d2b1f4fbb3ee9004257f155577d6e5cad4c307
TheDeathMuffin <samlongTGLM1@gmail.com>
2022-03-03 03:12:06 -0500

    Updated/merged JSON array.
    Merged JSON arrays into a singular array for naming/data consistency and centralization. Made many structural changes.

--- data.js
+++ data.js
@@ -1,168 +1,197 @@
 var pets=[
-			{
-				"name":"Wishbone"
-				"breed":"American Bulldog cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"age":"2Yrs 1Mths 2Wks (approx)"
-				"animal ID":"20579"
-			},
-
-			{
-				"name":"Benny"
-				"breed":"Australian Shepherd cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"age":"2Yrs 1Mths 4Wks (approx)"
-				"animal ID":"23531"
-			},
-
-			{
-				"name":"Cornelia"
-				"breed":"Boxer cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Female"
-				"age":"6Yrs 2Mthns 3Wks (approx)"
-				"animal ID":"23390"
-			},
-
-			{
-				"name":"Niko"
-				"breed":"Domestic Longhair cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"age":"3 Mths 3Wks"
-				"animal ID":"23541"
-			},
-
-			{
-				"name":"Howard"
-				"breed":"Domestic Short Hair Mixed Russian"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"age":""/*Age was not avaible*/
-				"animal ID":"23882"
-			},
-
-			{
-				"name":"Hemingway"
-				"breed":"Domestic Short Hair cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"age":"3Yrs 3Wks (approx)"
-				"animal ID":"23897"
-			}
-
-			];
-
-var details=[
-			{
-				"name":"Wishbone"
-				"Type":"Dog"
-				"breed":"American Bulldog cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"color":"Black"
-				"age":"2Yrs 1Mths 2Wks (approx)"
-				"size":"Large"
-				"health":"My health has been checked"
-				"vac":"My vaccinations are up to date"
-				"worming":"My worming is up to date"
-				"mircochip":"I have been microchipped"
-				"animal ID":"20579"
-				"micro Number":"985113004709947"
-				"bio":""
-			},
-
-
-			{
-				"name":"Benny"
-				"Type":"Dog"
-				"breed":"Australian Shepherd cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"color":"Brown / White"
-				"age":"2Yrs 1Mths 4Wks (approx)"
-				"health":"My health has been checked"
-				"vac":"My vaccinations are up to date"
-				"worming":"My worming is up to date"
-				"mircochip":"I have been microchipped" 
-				"animal ID":"23531"
-				"micro Number":"985113005872810"
-				"suit guide":"Not suitable for Children"
-			},
-
-			{
-				"name":"Cornelia"
-				"Type":"Dog"
-				"breed":"Boxer cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Female"
-				"color":"White"
-				"age":"6Yrs 2Mthns 3Wks (approx)"
-				"health":"My health has been checked"
-				"vac":"My vaccinations are up to date"
-				"worming":"My worming is up to date"
-				"mircochip":"I have been microchipped"
-				"animal ID":"23390"
-				"micro Number":"985113005872687"
-				"suit guide":"OK with Dogs" "Laid back" "Shy"/*need to figure out how to have these on the same line*/
-				"bio":""
-			},
-			{
-				"name":"Niko"
-				"Type":"Cat"
-				"breed":"Domestic Longhair cross"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"color":"Grey tabby / White"
-				"claw":"Declawed"
-				"age":"3 Mths 3Wks"
-				"health":"My health has been checked"
-				"vac":"My vaccinations are up to date"
-				"worming":"My worming is up to date"
-				"mircochip":"I have been microchipped"
-				"animal ID":"23541"
-				"micro Number":"985113006299812"
-				"bio":""
-			},
-
-			{
-				"name":"Howard"
-				"Type":"Cat"
-				"breed":"Domestic Short Hair Mixed Russian"
-				"second Breed":"Russian"
-				"spayed":"Spayed / Neutered"
-				"gender":"Male"
-				"color":"Grey"
-				"claw":"No"
-				"age":"Adult"
-				"health":"My health has been checked"
-				"vac":"My vaccinations are up to date"
-				"worming":"My worming is up to date"
-				"mircochip":"I have been microchipped"
-				"animal ID":"23882"
-				"mircochip Number":"985113006299853"
-				"bio":""
-			},
-
-			{
-				"name":"Hemingway"
-				"Type":"Cat"
-				"breed":"Domestic Short Hair cross"
-				"Spayed / Neutered":"Yes"
-				"gender":"Male"
-				"color":"Grey"
-				"claw":"No"
-				"age":"3Yrs 3Wks (approx)"
-				"health":"My health has been checked"
-				"vac":"My vaccinations are up to date"
-				"worming":"My worming is up to date"
-				"animal ID":"23897"
-				"suit guide":"Doesn't like Cats","Special Needs"
-				"bio":""
-			}
+	{
+	"name":"Wishbone",
+	"type":"Dog",
+	"breed":"American Bulldog cross",
+	"breed2":"",
+	"spayed":"Spayed / Neutered",
+	"gender":"Male",
+	"color":"Black",
+	"age":"2Yrs 1Mths 2Wks (approx)",
+	"size":"Large",
+	"health_checks":
+		[
+		{"check":"My health has been checked."},
+		{"check":"My vaccinations are up to date."},
+		{"check":"My worming is up to date."}
+		],
+	"sustainability_check":
+		[
+		],
+	"images":
+		[
+			{"img":"images/20579_1.jpg"},
+			{"img":"images/20579_2.jpg"},
+			{"img":"images/20579_3.jpg"},
+			{"img":"images/20579_4.jpg"},
+		],
+	"id":"20579",
+	"microchipped":true,
+	"micro_num":"985113004709947",
+	"bio":"I am a very intelligent neuroscientist: there is not a single case where a patient of mine has not been relieved of their symptoms. As a matter of fact, none of my clients have left a negative review of me! Some would say they end up in a frozen state of awe and amazement. Or...maybe they've just passed out. Either way, my job is done, and done well!"
+	},
+	{
+	"name":"Benny",
+	"type":"Dog",
+	"breed":"Australian Shepherd cross",
+	"breed2":"",
+	"spayed":"Spayed / Neutered",
+	"gender":"Male",
+	"color":"Brown / White",
+	"age":"2Yrs 1Mths 4Wks (approx)",
+	"size":"",
+	"health_checks":
+		[
+		{"check":"My health has been checked."},
+		{"check":"My vaccinations are up to date."},
+		{"check":"My worming is up to date."}
+		],
+	"sustainability_check":
+		[
+		{"check":"Not Suitable for Children"}
+		],
+	"images":
+		[
+			{"img":"images/23531_1.jpg"},
+			{"img":"images/23531_2.jpg"},
+			{"img":"images/23531_3.jpg"},
+			{"img":"images/23531_4.jpg"},
+		],
+	"id":"23531",
+	"microchipped":true,
+	"micro_num":"985113005872810",
+	"bio":"I am having a ruff day. I couple of colleages have been a real stick up my...They're barking up the wrong tree. They have a scratch I can't itch, they would be better off contacting the paw patrol."
+	},
+	{
+	"name":"Cornelia",
+	"type":"Dog",
+	"breed":"Boxer cross",
+	"breed2":"",
+	"spayed":"Spayed / Neutered",
+	"gender":"Female",
+	"color":"White",
+	"age":"6Yrs 2Mthns 3Wks (approx)",
+	"size":"",
+	"health_checks":
+		[
+		{"check":"My health has been checked."},
+		{"check":"My vaccinations are up to date."},
+		{"check":"My worming is up to date."}
+		],
+	"sustainability_check":
+		[
+		{"check":"OK with Dogs"},
+		{"check":"Laid back"},
+		{"check":"Shy"}
+		],
+	"images":
+		[
+			{"img":"images/23390_1.jpg"},
+			{"img":"images/23390_2.jpg"},
+			{"img":"images/23390_3.jpg"},
+			{"img":"images/23390_4.jpg"},
+		],
+	"id":"23390",
+	"microchipped":true,
+	"micro_num":"985113005872687",
+	"bio":"I am a fun-loving dorge. Doge. Duog. Dug. I don't know how to spell that. One thing I know, squirrels! What what??? Who said squirrels, those are my favorite!! Bark bark bark bark bark bark ruff howl ruff!"
+	},
+	{
+	"name":"Niko",
+	"type":"Cat",
+	"breed":"Domestic Longhair cross",
+	"breed2":"",
+	"spayed":"Spayed / Neutered",
+	"gender":"Male",
+	"color":"Grey tabby / White",
+	"claw":"Declawed",
+	"age":"3 Mths 3Wks",
+	"size":"",
+	"health_checks":
+		[
+		{"check":"My health has been checked."},
+		{"check":"My vaccinations are up to date."},
+		{"check":"My worming is up to date."}
+		],
+	"sustainability_check":
+		[
+		],
+	"images":
+		[
+			{"img":"images/23541_1.jpg"},
+			{"img":"images/23541_2.jpg"},
+			{"img":"images/23541_3.jpg"},
+			{"img":"images/23541_4.jpg"},
+		],
+	"id":"23541",
+	"microchipped":true,
+	"micro_num":"985113006299812",
+	"bio":"I am very literally a baby cat. I cannot even scar you at this point. Move along."
+	},
+	{
+	"name":"Howard",
+	"type":"Cat",
+	"breed":"Domestic Short Hair Mixed Russian",
+	"breed2":"Russian",
+	"spayed":"Spayed / Neutered",
+	"gender":"Male",
+	"color":"Grey",
+	"claw":"No",
+	"age":"Adult",
+	"size":"",
+	"health_checks":
+		[
+		{"check":"My health has been checked."},
+		{"check":"My vaccinations are up to date."},
+		{"check":"My worming is up to date."}
+		],
+	"sustainability_check":
+		[
+		],
+	"images":
+		[
+			{"img":"images/23882_1.jpg"},
+			{"img":"images/23882_2.jpg"},
+			{"img":"images/23882_3.jpg"},
+			{"img":"images/23882_4.jpg"},
+		],
+	"id":"23882",
+	"microchipped":true,
+	"micro_num":"985113006299853",
+	"bio":"I am just a cat. What else do you expect to see here? If you want some detail, check out those dogs. Those are the ones people actually care about :("
+	},
+	{
+	"name":"Hemingway",
+	"type":"Cat",
+	"breed":"Domestic Short Hair cross",
+	"breed2":"",
+	"spayed":"Spayed / Neutered",
+	"gender":"Male",
+	"color":"Grey",
+	"claw":"No",
+	"age":"3Yrs 3Wks (approx)",
+	"size":"",
+	"health_checks":
+		[
+		{"check":"My health has been checked."},
+		{"check":"My vaccinations are up to date."},
+		{"check":"My worming is up to date."}
+		],
+	"sustainability_check":
+		[
+		{"check":"Doesn't Like Cats"},
+		{"check":"Special Needs"}
+		],
+	"images":
+		[
+			{"img":"images/23897_1.jpg"},
+			{"img":"images/23897_2.jpg"},
+			{"img":"images/23897_3.jpg"},
+			{"img":"images/23897_4.jpg"},
+		],
+	"id":"23897",
+	"microchipped":false,
+	"micro_num":"",
+	"bio":"The only personality trait I have is being ornery."
+	}
 ];
-
-			
-
